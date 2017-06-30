8a31ec0e1c6ab54530d87413f85cf9b632b2f39c
lixxxiang <13051744716@163.com>
2017-06-30 09:03:26 +0800

    Initial Commit

---
+++ AnQuanBaoV2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AnQuanBaoV2
+//
+//  Created by yyfwptz on 2017/6/30.
+//  Copyright © 2017年 yyfwptz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AnQuanBaoV2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AnQuanBaoV2
+//
+//  Created by yyfwptz on 2017/6/30.
+//  Copyright © 2017年 yyfwptz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
