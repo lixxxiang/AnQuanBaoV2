ffb5f989e799b5347287af699f6146d9b26e9794
lixxxiang <13051744716@163.com>
2017-07-03 08:21:41 +0800

    first commit

---
+++ AnQuanBaoV2/CallPoliceView.h
@@ -0,0 +1,13 @@
+//
+//  CallPoliceView.h
+//  AnQuanBaoV2
+//
+//  Created by yyfwptz on 2017/6/30.
+//  Copyright © 2017年 yyfwptz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CallPoliceView : UIView
+
+@end
---
+++ AnQuanBaoV2/CallPoliceView.m
@@ -0,0 +1,21 @@
+//
+//  CallPoliceView.m
+//  AnQuanBaoV2
+//
+//  Created by yyfwptz on 2017/6/30.
+//  Copyright © 2017年 yyfwptz. All rights reserved.
+//
+
+#import "CallPoliceView.h"
+
+@implementation CallPoliceView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
